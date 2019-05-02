58a41319901232ae9037dccc4efefa68afcf9ced
Noriyuki <moto44.noriyuki@gmail.com>
2019-05-02 20:05:35 +0900

    Downshift再修正 (#12)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,23 @@
  * @fileoverview フォームテスト
  */
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { List } from 'immutable';
+import { UserRecord } from '../src/redux/models/User';
+
 import withRoot from '../src/hoc/withRoot';
 import TestForm from '../src/components/TestForm';
 import UsersActionDispatcher from '../src/redux/dispatchers/UsersActionDispatcher';
+import usersSelector from '../src/redux/selectors/users';
 
 import { InjectedProps as AuthInjectedProps } from '../src/hoc/withAuth';
 import { InjectedProps as PermissionInjectedProps } from '../src/hoc/withPermission';
+import { StateMap } from '../src/redux/State';
 
 // Props
-interface Props extends AuthInjectedProps, PermissionInjectedProps {}
+interface Props extends AuthInjectedProps, PermissionInjectedProps {
+  users: List<UserRecord>;
+}
 
 class Page extends React.Component<Props> {
   static async getInitialProps({ store }: any) {
@@ -19,15 +27,26 @@ class Page extends React.Component<Props> {
   }
 
   render() {
+    const { users } = this.props;
+    console.log(users);
     return (
-      <TestForm
-        initialCompanyId={1}
-        initialBeginDate={new Date()}
-        submit={value => {
-          console.log(JSON.stringify(value));
-        }}
-      />
+      <>
+        <TestForm
+          initialCompanyId={1}
+          initialBeginDate={new Date()}
+          submit={value => {
+            console.log(JSON.stringify(value));
+          }}
+        />
+      </>
     );
   }
 }
-export default withRoot({ permissions: ['VIEW'] })(Page);
+
+const mapStateToProps = (state: StateMap) => ({
+  users: usersSelector.getUsersResult(state)
+});
+
+export default connect(mapStateToProps)(
+  withRoot({ permissions: ['VIEW'] })(Page)
+);
